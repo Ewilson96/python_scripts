dbe0dfa32e3577c91779449a080f8653fddfdd36
Ewilson96 <ennil.wilson@outlook.com>
2026-06-03 14:48:17 -0400

    Update basic_checks.py

--- basic_checks.py
+++ basic_checks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import subprocess
 import paramiko
 from datetime import datetime
 
@@ -20,6 +20,11 @@ general_cmds = {
     "date": "date"
 }
 
+alma_cmds = {
+    "ssh_status":"systemctl is-active sshd",
+    "fw_status":"systemctl is-active firewalld"
+}
+
 ubuntu_cmds = {
     "cockpit_status": "systemctl is-active cockpit.socket",
     "port_status": "ss -tulpn | grep ':9090' | awk '{print $2}'"
@@ -37,7 +42,23 @@ now = datetime.now()
 print("\n")
 print(f"Timestamp: {now:%Y-%m-%d %H:%M:%S} EST")
 
+def ping_helper(servers):
+    ping_cmd = ["ping","-c","1","-W","1",servers]
+    ping_result = subprocess.run(ping_cmd,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+    return ping_result.returncode == 0
+
+reachable_servers = []
+
 for i in servers:
+    if ping_helper(i):
+        reachable_servers.append(i)
+        #print(f"{i} is up.")
+    else:
+        print(f"{i} is down.")
+
+print(f"\n*** Reachable VMs: {len(reachable_servers)}/{len(servers)} ***\n")      
+
+for i in reachable_servers:  
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -61,6 +82,8 @@ for i in servers:
             all_commands.update(controller_cmds)
         elif i == "ubuntu":
             all_commands.update(ubuntu_cmds)
+        elif i == "alma":
+            all_commands.update(alma_cmds)
 
         results = {}
 
@@ -90,11 +113,11 @@ for i in servers:
             print(f"PVE Daemon: {results['pvedaemon_status']}")
             print(f"PVE Cluster: {results['cluster_status']}")
 
-            print(f"\nHOST: {i} - FAILED SERVICES")
+            print(f"\nHOST: {i} - systemctl failed services")
             print("-" * 40)
             print(results["failed_services"])
 
-            print(f"\nHOST: {i} - RECENT ERROR LOGS")
+            print(f"\nHOST: {i} - journalctl recent error logs")
             print("-" * 40)
             print(results["error_logs"])
 
@@ -103,6 +126,12 @@ for i in servers:
             print("=" * 40)
             print(f"Remote Access - Service Status: {results['cockpit_status']}")
             print(f"Remote Access - Port Status: {results['port_status']}")
+
+        if i == "alma":
+            print(f"\nAlma Services")
+            print("=" * 40)
+            print(f"SSH daemon: {results['ssh_status']}")
+            print(f"Firewalld daemon: {results['fw_status']}")
         print("\n")
 
     except Exception as e:
