c392bdbebe62bbf5d587072e0eca9212be098c7c
Ewilson96 <ennil.wilson@outlook.com>
2026-06-21 02:31:06 -0400

    Refactor run_ansible_playbook parameters and logging

--- homelab.py
+++ homelab.py
@@ -7,16 +7,16 @@ import time
 logger = logging.getLogger(__name__)
 
 
-def progbar(iterable, desc="progress"):
-    return tqdm(iterable, desc=desc)
+# def progbar(iterable, desc="progress"):
+#     return tqdm(iterable, desc=desc)
 
 def run_ansible_playbook(
     playbook_name: str,
     playbook_dir: str,
     inventory_file: str = None,
-    #ask_pass: bool = False,
-    become: bool = False,
-    ask_pass: bool = False  #K option 
+    ask_ssh_pass: bool = False,   #-k
+    become: bool = False,         #-b
+    ask_become_pass: bool = False #-K option 
     ) -> tuple[bool, str]: # "This function returns a tuple (ordered sequence) containing a boolean and a string."
     """
     Returns:
@@ -25,6 +25,7 @@ def run_ansible_playbook(
             output (str)
     """
     playbook_path = Path(playbook_dir) / playbook_name
+    print(f"Running playbook: {playbook_path}")
 
     if not playbook_path.exists():
         raise FileNotFoundError(
@@ -34,14 +35,22 @@ def run_ansible_playbook(
     run_pb = ["ansible-playbook", str(playbook_path)] #list_01
 
     if inventory_file:
+        print(f"inventory found: {inventory_file}. Proceeding...")
         run_pb.extend(["-i", inventory_file]) #list_02 "ansible-playbook /home/ewilson/playbooks/patch.yml -i ../inventories/inventory.ini"
+        if ask_ssh_pass:
+            run_pb.append("-k")
+
+        if ask_become_pass:
+            run_pb.append("-K")
+
+        print(" ".join(run_pb))
 
     logger.info("run: %s", " ".join(run_pb)) # %s is a string placeholder for run_pb
 
     try:
         result = subprocess.run(
             run_pb,
-            capture_output=True,
+            capture_output=False,
             text=True,
             check=False
         )
@@ -61,13 +70,12 @@ def run_ansible_playbook(
     
 if __name__ == "__main__":
 
-    #tqdm stuff
-    pb = ["test.yml"]
-    for i in progbar(pb):
-        x,y = run_ansible_playbook(  #success/output
+    x,y = run_ansible_playbook(  #success/output
         playbook_name="test.yml", 
         playbook_dir="/home/ewilson/home_lab/ansible/playbooks",
         inventory_file="/home/ewilson/home_lab/ansible/inventories/inventory.ini",
+        ask_ssh_pass=True,
+        ask_become_pass=True,
         become=True
     )
 
